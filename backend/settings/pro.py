b0f6d3ec2cd5b6039c07e191770bd523ab71051a
xiaojuan2021 <xiaojuan2021@gmail.com>
2023-05-26 15:11:08 +0800

    fix: fic

--- backend/settings/pro.py
+++ backend/settings/pro.py
@@ -2,6 +2,6 @@ from .base import *
 DEBUG = False
 HOST = 'http://120.48.97.87/#/'
 ALLOWED_HOSTS = ['120.48.97.87']
-BROKER_URL = 'redis://120.48.97.87:6379/6'
+BROKER_URL = 'redis://127.0.0.1:6379/6'
 # BACKEND配置，这里使用redis
-CELERY_RESULT_BACKEND = 'redis://120.48.97.87:6379/6'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/6'
